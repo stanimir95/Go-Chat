66ef4f8a03c1d67999938e9b57e4e20a5560ea82
stanimir <dreamhack1122@gmail.com>
2019-02-08 07:27:24 +0200

    fixed typo

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
 			t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the aplication.")
@@ -30,7 +30,7 @@ func main() {
 	http.Handle("/room", r)
 	go r.run()
 	log.Println("Starting web server", *addr)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
